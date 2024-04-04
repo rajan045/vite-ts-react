065efd95208593b307493959da94f951dc0151cd
rajan <rajan@techosto.com>
2024-04-05 03:24:22 +0530

    sign up page starting

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -5,8 +5,14 @@ export default function Home() {
   return (
     <>
       <Header />
-      <div>
-
+      <video className="w-full h-screen object-cover fixed top-0 left-0 z-[-1]" autoPlay loop muted>
+        <source src="/images/astroids.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+      <div className="w-full h-screen flex items-center justify-center">
+        <form>
+          Hello
+        </form>
       </div>
       <Footer />
     </>
